360101f23a55312796b82b9717455f446b214a21
Sohrab momand <sohrabmomand.work@gmail.com>
2023-11-22 09:51:20 +0430

    building dashboard with react Js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// this line below will import the React library which is necessary for 
+// writing react and also JSX to wirite HMTL within javascript
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// the line below will import the ReactDOM library which provides methods 
+// for interacting with the DOM in React such as rendering React into DOM
+import ReactDom from 'react-dom';
+
+// the App component is the root component of the applicition
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// This line uses the ReactDOM.render() method to render the "App" component into the HTML element with the id "root" in the DOM.
+ReactDom.render(<App />, document.getElementById('root'));
